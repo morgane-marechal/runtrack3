ad65a1d649b29d85025c081296c92c97f41aa14a
morgane-marechal <morgane.marechal@laplateforme.io>
2023-01-17 11:29:47 +0100

    add folder jour01

---
+++ Jour01/Job05/script.js
@@ -0,0 +1,15 @@
+function afficherjourssemaine(){
+    var TableauSemaine = ["Lundi",
+				"Mardi",
+				"Mercredi",
+				"Jeudi",
+				"Vendredi",
+				"Samedi",
+				"Dimanche"];
+    for (var i in TableauSemaine)
+        {
+            console.log(TableauSemaine[i]);
+        }            
+}
+
+afficherjourssemaine();
